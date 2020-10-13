3a2a24c3dd3d4f21d70d39a2d1ab1d6b408ddee0
陈韫达 <422663079@qq.com>
2020-10-13 21:19:26 +0800

    feat: add config

---
+++ storybook-static/precache-manifest.a546b4ae8f3ba38170315c2af04b726b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32ff0e806ee05e363dfa5db47a69d8c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.40fc5737900131635099.bundle.js"
+  },
+  {
+    "url": "runtime~main.40fc5737900131635099.bundle.js"
+  },
+  {
+    "revision": "07703c5d10895b15e9ef",
+    "url": "static/css/main.13627036.chunk.css"
+  },
+  {
+    "url": "vendors~main.40fc5737900131635099.bundle.js"
+  },
+  {
+    "url": "vendors~main.40fc5737900131635099.bundle.js.LICENSE.txt"
+  }
+]);
